8feafaebe21ffc8117a79cba56e5c801e34f8d44
D3vmotiv <czechpiotr2701@gmail.com>
2021-09-07 00:34:45 +0200

    Fix linters setup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
   // This starter template is using Vue 3 <script setup> SFCs
   // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-  import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="w-screen">
+    <img class="mx-auto" alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
   </div>
 </template>
