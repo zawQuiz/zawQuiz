daa9a0c9fb14f0b4cb21914cfb22d9ce8150fe9c
D3vmotiv <czechpiotr2701@gmail.com>
2021-09-07 18:12:34 +0200

    Fixing github-husky hooks

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,8 +6,8 @@ import { resolve } from 'path'
 export default defineConfig({
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
-    }
+      '@': resolve(__dirname, 'src'),
+    },
   },
   plugins: [vue()],
 })
