6ce6f5640905732fdc841f95f91531004f736503
jakub-rudnik <jakub.rudnik@interia.pl>
2021-10-15 23:52:27 +0200

    setup theme and added font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,30 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      bg: "#22283A",
+      secondary: "#2A3349",
+      primary: {
+        DEFAULT: "#3D4557",
+        dark: "#323948",
+      },
+      green: {
+        DEFAULT: "#329A68",
+        dark: "#2C875B",
+      },
+      red: {
+        DEFAULT: "#9A3232",
+        dark: "#872C2C",
+      },
+      white: "#ffffff",
+      gray: "#C4C4C4",
+    },
+    fontFamily: {
+      'regular': ['Poppins'],
+      'bold': ['Poppins-Bold'],
+    },
     extend: {},
   },
   variants: {
