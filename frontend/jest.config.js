92271c3bf3bb122f08203e896e9389735a6f259e
jakub-rudnik <jakub.rudnik@interia.pl>
2021-10-24 18:20:16 +0200

    Fixed jest config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.vue$': 'vue3-jest',
