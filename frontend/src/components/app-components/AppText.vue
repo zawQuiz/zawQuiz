27db33822c4f438c6879015a943611038e07f08e
jakub-rudnik <jakub.rudnik@interia.pl>
2021-10-24 18:19:46 +0200

    Added Text Component

---
+++ frontend/src/components/app-components/AppText.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface ITextProps {
+    variant: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "span" | "p";
+    text: string;
+    bold?: boolean;
+    size: number;
+}
+
+const { bold, size, variant, text } = defineProps<ITextProps>()
+
+const fontBold = bold ? "font-bold" : ""
+
+</script>
+
+<template>
+    <variant :class="[fontBold]" :style="{ fontSize: `${size}px` }">{{ text }}</variant>
+</template>
