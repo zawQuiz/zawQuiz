eea57a9d7ccef1f2185f1fba72f6036c48491962
jakub-rudnik <jakub.rudnik@interia.pl>
2021-11-07 23:34:41 +0100

    Added Icon Component

---
+++ frontend/src/components/app-components/AppIcon.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+interface IIconProps {
+    filled?: boolean;
+    name: "home" | "swords" | "question" | "profile";
+}
+
+const { filled, name } = defineProps<IIconProps>()
+
+const source = filled ? `src/assets/icons/${name}_filled.svg` : `src/assets/icons/${name}.svg`
+</script>
+
+<template>
+    <img :src="source" />
+</template>
