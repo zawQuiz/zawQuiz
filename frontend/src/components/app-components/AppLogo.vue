19f1f6f0f3e22f8d07c219657954c9d820a8fbaf
jakub-rudnik <jakub.rudnik@interia.pl>
2021-11-07 15:27:52 +0100

    Added Logo Component

---
+++ frontend/src/components/app-components/AppLogo.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+interface ILogoProps {
+    small?: boolean;
+}
+
+const { small } = defineProps<ILogoProps>()
+
+const whiteText = small ? "z" : "zaw"
+const greenText = small ? "Q" : "Quiz"
+
+</script>
+
+<template>
+    <router-link to="/">
+        <p class="font-bold text-[36px]">
+            {{ whiteText }}
+            <!--
+            -->
+            <span class="text-green">{{ greenText }}</span>
+        </p>
+    </router-link>
+</template>
