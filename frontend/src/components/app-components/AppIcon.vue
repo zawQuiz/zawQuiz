3ffb60369a8844d4fb00166725d03b6282b83d8d
jakub-rudnik <jakub.rudnik@interia.pl>
2021-11-10 19:59:50 +0100

    changed prop name

--- frontend/src/components/app-components/AppIcon.vue
+++ frontend/src/components/app-components/AppIcon.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 interface IIconProps {
     filled?: boolean;
-    name: "home" | "swords" | "question" | "profile";
+    icon: "home" | "swords" | "question" | "profile";
 }
 
-const { filled, name } = defineProps<IIconProps>()
+const { filled, icon } = defineProps<IIconProps>()
 
-const source = filled ? `src/assets/icons/${name}_filled.svg` : `src/assets/icons/${name}.svg`
+const source = filled ? `src/assets/icons/${icon}_filled.svg` : `src/assets/icons/${icon}.svg`
 </script>
 
 <template>
