a77e24cd54349347d143f2efa1e0cd6ac1b086bd
jakub-rudnik <jakub.rudnik@interia.pl>
2021-11-12 17:39:21 +0100

    visual inspection of components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import AppButton from '@/components/app-components/AppButton.vue';
+import AppInput from '@/components/app-components/AppInput.vue';
+import AppLogo from '@/components/app-components/AppLogo.vue';
+import AppText from '@/components/app-components/AppText.vue';
+import AppIcon from '@/components/app-components/AppIcon.vue';
 
 function click() {
   alert('Hi!');
@@ -21,6 +25,23 @@ function click() {
 
       <p>Button Link:</p>
       <AppButton variant="solid" title="Button" link="/zaloguj" />
+
+      <AppInput name="text" label="Input" placeholder="placeholder" />
+
+      <AppLogo />
+
+      <AppText variant="h1" text="jajco" :size="30" />
+
+      <div class="flex">
+        <AppIcon icon="home" />
+        <AppIcon filled icon="home" />
+        <AppIcon icon="swords" />
+        <AppIcon filled icon="swords" />
+        <AppIcon icon="question" />
+        <AppIcon filled icon="question" />
+        <AppIcon icon="profile" />
+        <AppIcon filled icon="profile" />
+      </div>
     </div>
   </div>
 </template>
